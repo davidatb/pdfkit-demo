54aa327ad08e6f5acb4d4f1a864d938c189fc83e
davidatb <davidabrahamtellezbautista@hotmail.com>
2023-12-01 02:06:11 +0000

    feat(package.json): añadir la dependencia pdfkit-table para soporte de tablas en PDF refactor(pdfKit.js): cambiar la importación de pdfkit a pdfkit-table feat(pdfKit.js): añadir funcionalidad para crear tablas en PDF feat(index.js): implementar la generación de facturas en PDF en la ruta /invoice

--- src/libs/pdfKit.js
+++ src/libs/pdfKit.js
@@ -1,7 +1,21 @@
-import PDFDocument from "pdfkit";
+import PDFDocument from "pdfkit-table";
 
-function buildPDF(datacallback, endcallback) {
+export function buildPDF(datacallback, endcallback) {
   const doc = new PDFDocument();
   doc.on("data", datacallback);
   doc.on("end", endcallback);
+  doc.fontSize(25).text("Some text with an embedded font!", 100, 100);
+  doc.text("Here is some vector graphics...");
+
+  const tableArray = {
+    headers: ["Country", "Conversion rate", "Trend"],
+    rows: [
+      ["Switzerland", "12%", "+1.12%"],
+      ["France", "67%", "-0.98%"],
+      ["England", "33%", "+4.44%"],
+    ],
+  };
+  doc.table( tableArray, { width: 500 }); // A4 595.28 x 841.89 (portrait) (about width sizes)
+
+  doc.end();
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import { buildPDF } from "../libs/pdfKit.js";
 
 const router = Router();
 
-router.get("/invoice", (req, res) => {
-    res.send("Invoice");
+router.get("/invoice", (_, res) => {
+  
+  const stream = res.writeHead(200, {
+    "Content-Type": "application/pdf",
+    "Content-Disposition": 'attachment; filename="invoice.pdf"'
+  });
+
+  buildPDF(
+    (data) => { stream.write(data); },
+    () => { stream.end(); }
+  );
+
+  res.send("Invoice");
 });
 
 export default router;
